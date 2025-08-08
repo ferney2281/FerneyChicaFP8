06a64857d45bd30fce460d1df731867a5cab9805
ferney chica <ferney.chica@upb.edu.co>
2025-08-08 09:39:02 -0500

    N/A

--- 3.Condicionales/Program.cs
+++ 3.Condicionales/Program.cs
@@ -5,7 +5,7 @@ namespace _3.Condicionales
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
             //Condicionales simples
             /* 1.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos.*/
 
