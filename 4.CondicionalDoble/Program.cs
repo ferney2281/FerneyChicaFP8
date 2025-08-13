82e0ab1f803146b963a4318c6d5a5d20846b74c8
ferney2281 <ferney.chica@upb.edu.co>
2025-08-13 15:34:05 -0500

    Add files via upload
    Condicionales dobles

---
+++ 4.CondicionalDoble/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _4.CondicionalDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”. */
+
+           /* string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre=Console.ReadLine();
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if(sueldo>=3000)
+            {
+                //Verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Falsa
+                Console.WriteLine($"La persona {nombre}, no abona impuestos");
+            }*/
+
+
+
+
+        }
+    }
+}
