7f1510e0f1a492564f69b5ea4f44941f5f871834
ferney2281 <ferney.chica@upb.edu.co>
2025-08-29 15:39:41 -0500

    Add files via upload
    Ciclo While

---
+++ 10.While2/10.While2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5! = 1*2*3*4*5)
+            int num = 0;
+            int contador = 1;
+            int acumulador = 1;
+            Console.WriteLine("Ingrese un número para calcular su factorial:");
+            num=int.Parse(Console.ReadLine());
+
+            while (contador<=num)
+            {
+                acumulador=acumulador*contador;
+                Console.WriteLine($"Contador: {contador} - Factorial: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"El factorial del número {num}, es: {acumulador}");
+        }
+    }
+}
